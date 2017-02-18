536ee39c3b8b0e1114dead88fe8c96ac06316861
leavesster <levesster@gmail.com>
2017-02-18 23:36:00 +0800

    let - state

--- src/components/classroomvideo/VideoIndex.js
+++ src/components/classroomvideo/VideoIndex.js
@@ -3,19 +3,24 @@ import {render} from 'react-dom';
 import RemoteVideo from './RemoteVideo';
 import AgoraRTC from './AgoraRTCSDK-1.8.0';
 
-let channel = "123";
-let AppKey = "b7c2835c0fc941e480664d982f9dd88a";
-let client = AgoraRTC.createRtcClient();
-let uid = "teacher";
-let disableAudio = false;
-let disableVideo = false;
-let localStream;
-
 class VideoIndex extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        disableAudio: false,
+        disableVideo: false,
+        channel: "123",
+        AppKey: "b7c2835c0fc941e480664d982f9dd88a",
+        client: AgoraRTC.createRtcClient(),
+        uid: "teacher",
+        localStream:null,
+      };
+    }
+
     componentWillMount(){
         let role = this.props.role;
-        localStream = AgoraRTC.createStream({
-            streamID: uid,
+        this.setState({localStream : AgoraRTC.createStream({
+            streamID:  this.state.uid,
             audio: true,
             // cameraId: videoSource.value, // 可选
             // microphoneId: audioSource.value, // 可选
@@ -24,15 +29,16 @@ class VideoIndex extends Component {
             attributes:{
                 role:role
             }
-        });
+          })
+        })
         // 设置分辨率
-        localStream.setVideoProfile("120P");
+        this.state.localStream.setVideoProfile("120P");
 
     }
     componentDidMount() {
         let role = this.props.role;
         // 报错监听
-        client.on('error', function(err) {
+        this.state.client.on('error', function(err) {
             console.log(err);
             if (err.reason === 'INVALID_CHANNEL_NAME') {
                 alert("Invalid channel name, Chinese characters are not allowed in channel name.");
@@ -40,12 +46,13 @@ class VideoIndex extends Component {
             }
         });
 
+        let fack = this;
         // 初始化一个流
-        client.init(AppKey,function(){
+        this.state.client.init(AppKey,function(){
             console.log("AgoraRTC client initialized");
-            client.join(channel, uid, function(uid) {
-                localStream = AgoraRTC.createStream({
-                    streamID: uid,
+            fack.state.client.join(fack.state.channel, fack.state.uid, function(uid) {
+                fack.state.localStream = AgoraRTC.createStream({
+                    streamID: fack.state.uid,
                     audio: true,
                     // cameraId: videoSource.value, // 可选
                     // microphoneId: audioSource.value, // 可选
@@ -56,19 +63,19 @@ class VideoIndex extends Component {
                     }
                 });
                 // 初始化流
-                localStream.init(function(){
+                fack.state.localStream.init(function(){
                     console.log("Get UserMedia successfully");
                     // displayStream('66666', localStream,"100", "100", '',"");
-                    localStream.play("mainView");
+                    fack.state.localStream.play("mainView");
                     console.log("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
                     // 将本地流推出去
-                    client.publish(localStream, function(err) {
+                    fack.state.client.publish(fack.state.localStream, function(err) {
                         console.log("Timestamp: " + Date.now());
                         console.log("Publish local stream error: " + err);
                     });
                 });
 
-                console.log("User " + uid + " join channel successfully"); // 加入房间成功回调
+                console.log("User " + fack.state.uid + " join channel successfully"); // 加入房间成功回调
                 console.log("Timestamp: " + Date.now());
             }, function(err){
                 console.log(err); // 加入房间失败回调
@@ -76,26 +83,26 @@ class VideoIndex extends Component {
         });
     }
     toggleVideo(){
-        disableVideo = !disableVideo;
-        if (disableVideo == true){
-            localStream.disableVideo();
+      this.setState({disableVideo: !this.state.disableVideo});
+        if (this.state.disableVideo == true){
+          this.state.localStream.disableVideo();
         }else {
-            localStream.enableVideo();
+          this.state.localStream.enableVideo();
         }
     }
     toggleAudio(){
-        disableAudio = !disableAudio;
-        if (disableAudio == true){
-            localStream.disableAudio();
-        }else {
-            localStream.enableAudio();
-        }
+      this.setState({disableAudio:!this.state.disableAudio})
+      if (this.state.disableAudio == true){
+        this.state.localStream.disableAudio();
+      } else {
+        this.state.localStream.enableAudio();
+      }
     }
     closeVideo(){
-        localStream.close();
+        this.state.localStream.close();
     }
     getStream(){
-        return localStream;
+        return this.state.localStream;
     }
     studentSpeak(){
         console.log('apply speak');
@@ -124,9 +131,9 @@ class VideoIndex extends Component {
         return (
             <div className="video-index">
                 <div id="mainView" className="video-view" ></div>
-                <RemoteVideo client={client} uid={ uid }
-                             closeVideo ={ this.closeVideo}
-                             getStream = {this.getStream} />
+                <RemoteVideo client={obj.state.client} uid={ obj.state.uid }
+                             closeVideo ={obj.closeVideo}
+                             getStream = {obj.getStream} />
                 {panel}
             </div>
         )
